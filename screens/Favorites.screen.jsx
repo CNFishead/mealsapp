3ae7c6b5e047caffc5414f92e3a544e1bd697601
Austin Howard <cnfishead@hotmail.com>
2023-12-26 18:35:01 -0500

    show message if no favorites

--- screens/Favorites.screen.jsx
+++ screens/Favorites.screen.jsx
@@ -4,6 +4,7 @@ import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 import MealItem from "../components/mealItem/MealItem.component";
 import MealsList from "../components/mealsList/MealsList.component";
+import colors from "../constants/colors";
 
 const FavoritesScreen = ({ navigation, route }) => {
   const [meals, setMeals] = useState([]);
@@ -14,6 +15,13 @@ const FavoritesScreen = ({ navigation, route }) => {
   // get the meals from the MEALS array that match the favorites
   const mealsToDisplay = MEALS.filter((meal) => favorites.some((item) => item === meal.id));
 
+  if (mealsToDisplay.length === 0 || !mealsToDisplay) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>No Favorites Found</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <MealsList meals={mealsToDisplay} navigation={navigation} route={route} />
@@ -26,5 +34,13 @@ export default FavoritesScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    // fontFamily: "open-sans-bold",
+    fontSize: 20,
+    color: colors.secondary,
+    fontWeight: "bold",
   },
 });
